3eed1186fd6a149f301d16388ab048ec14bdbc8d
kbui314 <kbui314@yahoo.com>
2019-07-08 13:27:17 -0500

    Added menu to main

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,11 +1,14 @@
 package com.revature;
 
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.display();
+        
     }
 }
